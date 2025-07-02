53d65a22d257ea16eff8142cbc810ccadfba6371
Louis-Bdc <louis.bourgault2009@gmail.com>
2025-07-02 14:40:23 +0800

    added vercel analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
 	import '../app.css';
+	import { injectAnalytics } from '@vercel/analytics/sveltekit'
+	import { dev } from '$app/environment';
 
 	let { children } = $props();
+	injectAnalytics({ mode: dev ? 'development' : 'production' });
 </script>
 
 {@render children()}
